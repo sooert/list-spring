53a12cede18b16b1a56e2cbc889d02a1aa4f6cdb
sooert <nr7077@naver.com>
2024-12-11 07:40:49 +0900

    최종

--- src/main/resources/static/js/admin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    // 유저 전체 찾기
-    $('#find-all-btn').on('click', function() {
-        var con = confirm('유저 전체 찾기를 진행하시겠습니까?');
-        if(!con) return;
-        location.href = `./user/findAll`;
-    });
-});
